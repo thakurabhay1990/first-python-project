d1d670828ff560f935a1fa610bbf362870556527
thakurabhay1990 <thakurabhay1990@gmail.com>
2023-05-09 23:27:34 +0530

    added 1 more program
    :

---
+++ calculate_simple_interest.py
@@ -0,0 +1,5 @@
+def calculate_simple_interest(principle, interest, duration):
+    return principle * (1 + interest * 0.01 * duration)
+
+
+print(calculate_simple_interest(2300000, 9.50, 5))
--- multiplication_table.py
+++ multiplication_table.py
@@ -2,4 +2,6 @@ def print_multiplication_table(table,start=5,end=10):
     for i in range(start,end+1):
         print(f"{table} * {i} = {table*i}")
 
-print_multiplication_table(23)
+print_multiplication_table(14)
+
+# TODO: Make sure to learn about for in depth.

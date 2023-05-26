8175522208430149b56ca743f818910794dc43c7
NEWUSER <thakurabhay1990@gmail.com>
2023-05-26 13:19:34 +0530

    understanding functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+# In Python, function is a group of related statements that perform a specific task.
+
+# Empty Function
+def GreetMe():
+    print("Good morning")
+GreetMe()
+
+# Function with argument
+def GreetMe(name):
+    print("Good morning " + name)
+GreetMe("Abhay Thakur")
+
+# Function that return sum of 2 numbers
+def AddIntegers(a, b):
+    return a + b
+
+print(AddIntegers(2, 5))

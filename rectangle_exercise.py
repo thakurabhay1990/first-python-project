2c0041fe3bc2f4236b63ee5494bbb60b5c4aa179
NEWUSER <thakurabhay1990@gmail.com>
2023-09-07 12:22:17 +0530

    Area & Circumference of Rectangle

--- rectangle_exercise.py
+++ rectangle_exercise.py
@@ -12,6 +12,6 @@ class Rectangle:
         return (2 * self.l) + (2 * self.b)
 
 
-rect1 = Rectangle(2,4)
+rect1 = Rectangle(2, 4)
 print(f"Area of Rectangle is {rect1.area_of_rectangle()}")
 print(f"Circumference of Rectangle is {rect1.circum_of_rectangle()}")
